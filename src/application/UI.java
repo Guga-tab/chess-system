aa7c0c9c66fc03e5c37b17d89ca1dcfa3ca027d7
Gustavo Machado <gustavomachado.521@gmail.com>
2026-01-09 18:26:58 -0300

    Fix: UI prototype

--- src/application/UI.java
+++ src/application/UI.java
@@ -13,10 +13,15 @@ public class UI {
     public void print(){
         int qtRow = board.getRows();
         int qtColumns = board.getColums();
-        for (int i = 0; i < qtRow; i++) {
+        for (int i = 0; i <= qtRow; i++) {
+            if(i == 8){
+                System.out.println("  a b c d e f g h");
+                break;
+            }
             System.out.print(i + 1 + " ");
             for (int j = 0; j < qtColumns; j++) {
                 if (board.piece(i, j) != null){
+                    //Lógica para imprimir o Símbolo/Letra da peça
                     System.out.print("P ");
                 }else{
                     System.out.print("- ");
